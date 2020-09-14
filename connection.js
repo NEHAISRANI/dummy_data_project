c84309588c2e58857a157cd20d64da7430cda965
NEHAISRANI <nehai18@navgurukul.org>
2020-09-14 12:57:43 +0530

    added connectionn file in connection file given information regarding database

---
+++ connection.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+      host : process.env.DB_HOST,
+      user : process.env.DB_USER, 
+      password : process.env.DB_PASS,
+      database : process.env.DB_NAME
+    }
+  }); 
+
+
+
+module.exports = knex ;
+// console.log( process.env.DB_HOST,process.env.DB_USER,process.env.DB_PASS,process.env.DB_NAME)
+
